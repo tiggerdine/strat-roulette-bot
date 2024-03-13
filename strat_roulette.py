3de54cb4335f1d6a25881236433bc06a167aa282
Martin <tiggerdine@gmail.com>
2024-03-13 10:51:55 +0000

    Get strat for map and side

--- strat_roulette.py
+++ strat_roulette.py
@@ -7,24 +7,21 @@ options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.get("https://strat-roulette.github.io")
 
-ct_strat_button = driver.find_element(By.XPATH, "//button[text()='CT Strat']")
 
-
-# TODO get_strat(map, side)
-def get_strat(map):
+def get_strat(map, side):
     try:
         driver.find_element(By.XPATH, "//input[@value='{}']".format(map)).click()
     except NoSuchElementException:
         driver.find_element(By.XPATH, "//input[@value='null']").click()
 
-    ct_strat_button.click()
+    driver.find_element(By.XPATH, "//button[text()='{} Strat']".format(side)).click()
     title = driver.find_element(By.ID, "title")
     desc = driver.find_element(By.ID, "desc")
     return {"title": title.text, "desc": desc.text}
 
 
-for i in range(3):
-    strat = get_strat("dust2")
+for i in range(10):
+    strat = get_strat("dust2", "T")
     print(strat)
 
 driver.quit()
