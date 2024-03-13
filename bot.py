14480dd7dc21af8f600e85909efe2c803905201c
Martin <tiggerdine@gmail.com>
2024-03-13 12:04:12 +0000

    Consume GSI data

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import os
 
 import discord
-from flask import Flask
+from flask import Flask, request
 
 from scraper import get_strat
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["POST"])
+def consume():
+    data = request.get_json()
+    print(data)
+    return ""
 
 
 class StratRouletteBot(discord.Client):
