97542b03248ebc75b3ffa547d1f9ebc47ceb3c97
Martin <tiggerdine@gmail.com>
2024-03-13 00:00:23 +0000

    Get a few strats

--- strat_roulette.py
+++ strat_roulette.py
@@ -7,12 +7,17 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://strat-roulette.github.io")
 
 ct_strat_button = driver.find_element(By.XPATH, "//button[text()='CT Strat']")
-ct_strat_button.click()
 
-title = driver.find_element(By.ID, "title")
-print(title.text)
 
-desc = driver.find_element(By.ID, "desc")
-print(desc.text)
+def get_strat():
+    ct_strat_button.click()
+    title = driver.find_element(By.ID, "title")
+    desc = driver.find_element(By.ID, "desc")
+    return {"title": title.text, "desc": desc.text}
+
+
+for i in range(3):
+    strat = get_strat()
+    print(strat)
 
 driver.quit()
