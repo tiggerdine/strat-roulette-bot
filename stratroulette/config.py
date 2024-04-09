ea5c83570aa0e828834b9d15eba34d68089dc598
Martin <tiggerdine@gmail.com>
2024-04-09 18:20:05 +0100

    Move config files to their own folder

--- stratroulette/config.py
+++ stratroulette/config.py
@@ -2,8 +2,6 @@ import configparser
 import os
 
 config = configparser.ConfigParser()
-config.read("../config.ini")
-
+config.read("../config/config.ini")
 bot_token = config.get("Bot", "token", vars=os.environ)
-
 gsi_token = config.get("GSI", "token")
