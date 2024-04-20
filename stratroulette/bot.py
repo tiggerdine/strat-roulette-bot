d6a107be24810ad69137b5207d7a28e053863cf3
Martin <tiggerdine@gmail.com>
2024-04-20 16:03:00 +0100

    Move more logic to bot.py

--- stratroulette/bot.py
+++ stratroulette/bot.py
@@ -5,7 +5,8 @@ import nest_asyncio
 from flask import Flask, request
 
 from stratroulette.config import bot_token, gsi_token
-from stratroulette.strats import get_strat_if_freezetime
+from stratroulette.gsi import verify_token, is_freezetime, get_map, get_team
+from stratroulette.strats import get_strat
 
 nest_asyncio.apply()
 
@@ -32,10 +33,24 @@ class StratRouletteBot(discord.Client):
 
 bot = StratRouletteBot(intents=discord.Intents.default())
 
-t1 = Thread(target=lambda: bot.run(bot_token))
-t2 = Thread(target=lambda: app.run())
+
+def get_strat_if_freezetime(data, token):
+    if verify_token(data, token) and is_freezetime(data):
+        mapp = get_map(data)
+        team = get_team(data)
+        strat = get_strat(mapp, team)
+        formatted_strat = format_strat(strat)
+        return formatted_strat
+
+
+def format_strat(strat):
+    return "# {}\n{}".format(strat["title"], strat["desc"])
+
 
 if __name__ == "__main__":
+    t1 = Thread(target=lambda: bot.run(bot_token))
+    t2 = Thread(target=lambda: app.run())
+
     t1.start()
     t2.start()
     t1.join()
--- stratroulette/strats.py
+++ stratroulette/strats.py
@@ -2,21 +2,10 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-from stratroulette.gsi import is_freezetime, get_map, get_team, verify_token
-
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.get("https://strat-roulette.github.io")
-
-
-def get_strat_if_freezetime(data, gsi_token):
-    if verify_token(data, gsi_token) and is_freezetime(data):
-        mapp = get_map(data)
-        team = get_team(data)
-        strat = get_strat(mapp, team)
-        formatted_strat = format_strat(strat)
-        return formatted_strat
 
 
 def get_strat(mapp, team):
@@ -37,7 +26,3 @@ def find_map_button(mapp):
 
 def find_team_button(team):
     return driver.find_element(By.XPATH, "//button[text()='{} Strat']".format(team))
-
-
-def format_strat(strat):
-    return "# {}\n{}".format(strat["title"], strat["desc"])
