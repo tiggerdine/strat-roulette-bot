22b339cef3ce86a52731a4fa0f140eb7a1885e4e
Martin <tiggerdine@gmail.com>
2024-05-05 16:15:52 +0100

    Blacklist the "Double or Nothing" strat

--- stratroulette/strats.py
+++ stratroulette/strats.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+BLACKLIST = ["Double or Nothing"]
+
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
@@ -14,9 +16,13 @@ def generate_strat(mapp, team):
     except NoSuchElementException:
         driver.find_element(By.XPATH, "//input[@value='null']").click()
 
-    find_team_button(team).click()
-    title = driver.find_element(By.ID, "title")
-    desc = driver.find_element(By.ID, "desc")
+    team_button = find_team_button(team)
+    while True:
+        team_button.click()
+        title = driver.find_element(By.ID, "title")
+        if title not in BLACKLIST:
+            desc = driver.find_element(By.ID, "desc")
+            break
     return {"title": title.text, "desc": desc.text}
 
 
